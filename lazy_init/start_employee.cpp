cf3fbef24ba87ec13b2a0b89c16e585039788025
fenbf <bartlomiej.filipek@gmail.com>
2019-10-22 09:02:01 +0200

    some extra ops

--- lazy_init/start_employee.cpp
+++ lazy_init/start_employee.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <numeric>
 
 class CompanyDatabase {
 public:
     int GenerateID() { return _nextAvailableID++; }
+    size_t MaxEntries() const { return s_table.size(); }
     
     std::string FetchName(int id) const    { return s_table.at(id)._name; }
     std::string FetchSurname(int id) const { return s_table.at(id)._surname; }
-    std::string FetchAddress(int id) const { return s_table.at(id)._address; }
+    std::string FetchCity(int id) const { return s_table.at(id)._city; }
     double      FetchSalary(int id) const  { return s_table.at(id)._salary; }
 private:
     int _nextAvailableID { 0 };
     
-    struct EmployeeRecord { std::string _name; std::string _surname; std::string _address; double _salary; };
+    struct EmployeeRecord { std::string _name; std::string _surname; std::string _city; double _salary; };
     
     static inline const std::vector<EmployeeRecord> s_table {
-        { "John", "Doe", "Poland, ABC Street 67", 100.00 },
-        { "John", "Doe", "Poland, ABC Street 67", 100.00 },
-        { "John", "Doe", "Poland, ABC Street 67", 100.00 },
-        { "John", "Doe", "Poland, ABC Street 67", 100.00 }
+        { "John", "Doe", "Cracow", 120.00 },
+        { "Kate", "Doe", "Cracow", 80.00 },
+        { "Linda", "Doe", "Warsaw", 200.00 },
+        { "Marc", "Doe", "Warsaw", 100.00 }
     };
 };
 
@@ -30,14 +33,19 @@ public:
     void Initialise(CompanyDatabase& db) {
          _name = db.FetchName(_id);
          _surname = db.FetchSurname(_id);
-         _address = db.FetchAddress(_id);
+         _city = db.FetchCity(_id);
          _salary = db.FetchSalary(_id);
         _isInitialised = true;
     }
     
+    bool IsInitialised() const { return _isInitialised; }
+    
+    std::string City() const { return _city; }
+    double Salary() const { return _salary; }
+    
     friend std::ostream& operator<<(std::ostream& os, const Employee& em)
     {
-        os << em._id << ": " << em._name << ", " << em._surname << ", " << em._address << ", " << em._salary << '\n';
+        os << em._id << ": " << em._name << ", " << em._surname << ", " << em._city << ", " << em._salary << '\n';
         return os;
     }
 private:
@@ -45,13 +53,34 @@ private:
     int _id { -1 };
     std::string _name;
     std::string _surname;
-    std::string _address;
+    std::string _city;
     double _salary { 0.0 };
 };
 
 int main() {
     CompanyDatabase db;
-    Employee john(db.GenerateID());
-    john.Initialise(db);
-    std::cout << john;
+    std::vector<Employee> workers;
+    for (size_t i = 0; i < db.MaxEntries(); ++i)
+        workers.emplace_back(Employee { db.GenerateID() });
+        
+    auto SalaryOp = [&db](double curr, Employee& em) {
+        if (!em.IsInitialised())
+            em.Initialise(db);
+        return curr + em.Salary();
+    };
+    const double sumSalary = std::accumulate(std::begin(workers), std::end(workers), 0.0, SalaryOp); 
+    std::cout << "Sum salary: " << sumSalary << '\n';
+    
+    // by cities:
+    std::map<std::string, int> mapByCity;
+    for (auto& em : workers)
+    {
+        if (!em.IsInitialised())
+            em.Initialise(db);
+            
+        mapByCity[em.City()]++;
+    }
+    
+    for (const auto&[city, num] : mapByCity)
+        std::cout << city << ": " << num << '\n';
 }
