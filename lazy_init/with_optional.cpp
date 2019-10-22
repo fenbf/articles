b89cea011a985cb5389915a8bcd43bf92818f1bc
fenbf <bartlomiej.filipek@gmail.com>
2019-10-22 11:25:05 +0200

    new class that contains optional as a member

--- lazy_init/with_optional.cpp
+++ lazy_init/with_optional.cpp
@@ -8,17 +8,22 @@
 
 class CompanyDatabase {
 public:
+    struct EmployeeRecord { std::string _name; std::string _surname; std::string _city; double _salary; };    
+    
+public:
     int GenerateID() { return _nextAvailableID++; }
     size_t MaxEntries() const { return s_table.size(); }
     
+    // we use .at so that it might throw...
     std::string FetchName(int id) const    { return s_table.at(id)._name; }
     std::string FetchSurname(int id) const { return s_table.at(id)._surname; }
     std::string FetchCity(int id) const { return s_table.at(id)._city; }
     double      FetchSalary(int id) const  { return s_table.at(id)._salary; }
+    
+    EmployeeRecord FetchRecord(int id) const { return s_table.at(id); }
+    
 private:
     int _nextAvailableID { 0 };
-    
-    struct EmployeeRecord { std::string _name; std::string _surname; std::string _city; double _salary; };
     
     static inline const std::vector<EmployeeRecord> s_table {
         { "John", "Doe", "Cracow", 120.00 },
@@ -30,7 +35,7 @@ private:
 
 class Employee {
 public:
-    Employee(int id) : _id(id) { } // not initialised!
+    explicit Employee(int id) : _id(id) { } // not initialised!
     
     void Initialise(CompanyDatabase& db) {
          _name = db.FetchName(_id);
@@ -112,7 +117,7 @@ private:
     double _salary { 0.0 };
 };
 
-void OptionalTest() {
+void InitedTest() {
     CompanyDatabase db;
     std::vector<std::pair<int, std::optional<EmployeeInited>>> workers;
     for (size_t i = 0; i < db.MaxEntries(); ++i)
@@ -140,7 +145,59 @@ void OptionalTest() {
         std::cout << city << ": " << num << '\n';
 }
 
+class EmployeeOpt {
+public:
+    explicit EmployeeOpt(int id) : _id(id) { } 
+    
+    void Initialise(CompanyDatabase& db) {
+         _rec = db.FetchRecord(_id);
+    }
+    
+    bool IsInitialised() const { return _rec.has_value(); }
+    
+    std::string City() const { return _rec->_city; }
+    double Salary() const { return _rec->_salary; }
+    
+    friend std::ostream& operator<<(std::ostream& os, const EmployeeOpt& em)
+    {
+        os << em._id << ": " << em._rec->_name << ", " << em._rec->_surname << ", " << em._rec->_city << ", " << em._rec->_salary << '\n';
+        return os;
+    }
+private:
+    int _id { -1 };
+    std::optional<CompanyDatabase::EmployeeRecord> _rec;
+};
+
+void OptionalTest() {
+    CompanyDatabase db;
+    std::vector<EmployeeOpt> workers;
+    for (size_t i = 0; i < db.MaxEntries(); ++i)
+         workers.emplace_back(EmployeeOpt { db.GenerateID() });
+        
+    auto SalaryOp = [&db](double curr, EmployeeOpt& em) {
+        if (!em.IsInitialised())
+            em.Initialise(db);
+        return curr + em.Salary();
+    };
+    const double sumSalary = std::accumulate(std::begin(workers), std::end(workers), 0.0, SalaryOp); 
+    std::cout << "Sum salary: " << sumSalary << '\n';
+    
+    // by cities:
+    std::map<std::string, int> mapByCity;
+    for (auto& em : workers)
+    {
+        if (!em.IsInitialised())
+            em.Initialise(db);
+            
+        mapByCity[em.City()]++;
+    }
+    
+    for (const auto&[city, num] : mapByCity)
+        std::cout << city << ": " << num << '\n';
+}
+
 int main() {
     InitTest();
+    InitedTest();
     OptionalTest();
 }
