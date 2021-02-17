e78687aae5ebeac6c23c498603aa57432116136b
fenbf <bartlomiej.filipek@gmail.com>
2021-02-17 14:25:35 +0100

    benchmarks

--- filterElements/filters.cpp
+++ filterElements/filters.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <execution>
 #include <functional>
+#include <future>
 #include <iostream>
 #include <memory>
 #include <ranges>
@@ -9,8 +10,15 @@
 #include <vector>
 #include <type_traits>
 
-// filter - copy only those elements into out that satisfies the predicate
+#include "simpleperf.h"
 
+// filter - copy only those elements into out that satisfies the predicate
+template <typename TContainer>
+void Print(std::string_view intro, const TContainer& container) {
+	std::cout << intro << ": ";
+	for (size_t i = 0; auto && elem : container)
+		std::cout << elem << (++i == container.size() ? "\n" : ", ");
+};
 
 // vec and out won't ever overlap
 // but you can vec = Filter(vec)
@@ -69,27 +77,162 @@ auto FilterCopyIfConcepts(const std::vector<T>& vec, Pred p) {
 template <typename T, typename Pred>
 auto FilterCopyIfPar(const std::vector<T>& vec, Pred p) {
 	std::vector<T> out;
-	// error C2338: Parallel algorithms require forward iterators or stronger.
-	// VS 2019 16.8
-	// copy each satisfying _Pred
-	// not parallelized at present, parallelism expected to be feasible in a future release
-	//std::copy_if(std::execution::par, begin(vec), end(vec), std::back_inserter(out), p);
 	std::mutex mut;
 	std::for_each(std::execution::par, begin(vec), end(vec),
 		[&out, &mut, p](auto&& elem) {
-			if (p(elem))
-			{
+			if (p(elem)) {
 				std::unique_lock lock(mut);
 				out.push_back(elem);
 			}
 		});
 
-	// other options:
-	// helper vector with 1 and 0, 1 when we should copy
-	// another for each loop and put elements of 1 in a proper destination exlusive_scan
-	// create out vector of the size scan[last]+1 (probably)
-	// another loop to copy elements into positions from that scan
+	return out;
+}
 
+template <typename T, typename Pred>
+auto FilterCopyIfParCompose(const std::vector<T>& vec, Pred p) {
+	std::vector<uint32_t> buffer(vec.size());
+	std::vector<uint32_t> idx(vec.size());
+	std::transform(std::execution::par, begin(vec), end(vec), begin(buffer), [&p](const T& elem) {
+		return p(elem) ? 1 : 0;
+	});
+
+	//Print("buffer", buffer);
+	std::exclusive_scan(std::execution::par, begin(buffer), end(buffer), begin(idx), 0);
+	//Print("idx", idx);
+
+	std::vector<T> out(idx.back());
+	std::vector<size_t> indexes(vec.size());
+	std::iota(indexes.begin(), indexes.end(), 0);
+
+	std::for_each(std::execution::par, begin(indexes), end(indexes), [&buffer, &vec, &idx, &out](size_t i) {
+		if (buffer[i])
+		{
+			out[idx[i]] = vec[i];
+		}
+	});
+
+
+	//Print("out", out);
+	return out;
+}
+
+template <typename T, typename Pred>
+auto FilterCopyIfParComposeSeq(const std::vector<T>& vec, Pred p) {
+	std::vector<uint32_t> buffer(vec.size());
+	std::vector<uint32_t> idx(vec.size());
+	std::transform(std::execution::par, begin(vec), end(vec), begin(buffer), [&p](const T& elem) {
+		return p(elem) ? 1 : 0;
+		});
+
+	//Print("buffer", buffer);
+	std::exclusive_scan(std::execution::par, begin(buffer), end(buffer), begin(idx), 0);
+	//Print("idx", idx);
+
+	std::vector<T> out(idx.back());
+	//std::vector<size_t> indexes(vec.size());
+	//std::iota(indexes.begin(), indexes.end(), 0);
+
+	for (size_t i = 0; i < vec.size(); ++i)
+	{
+		if (buffer[i])
+		{
+			out[idx[i]] = vec[i];
+		}
+	}
+
+	//Print("out", out);
+	return out;
+}
+
+template <typename T, typename Pred>
+auto FilterCopyIfParChunks(const std::vector<T>& vec, Pred p) {
+	const auto chunks = std::thread::hardware_concurrency();
+	const auto chunkLen = vec.size() / chunks;
+	std::vector<size_t> indexes(chunks);
+	std::iota(indexes.begin(), indexes.end(), 0);
+
+	std::vector<std::vector<T>> copiedChunks(chunks);
+
+	std::for_each(std::execution::par, begin(indexes), end(indexes), [&](size_t i) {
+		auto startIt = std::next(std::begin(vec), i * chunkLen);
+		auto endIt = std::next(startIt, chunkLen);
+		std::copy_if(startIt, endIt, std::back_inserter(copiedChunks[i]), p);
+	});
+
+	std::vector<T> out;
+
+	for (const auto& part : copiedChunks)
+		out.insert(out.end(), part.begin(), part.end());
+
+	// remaining part:
+	if (vec.size() % chunks != 0) {
+		auto startIt = std::next(std::begin(vec), chunks * chunkLen);
+		std::copy_if(startIt, end(vec), std::back_inserter(out), p);
+	}
+
+	return out;
+}
+
+template <typename T, typename Pred>
+auto FilterCopyIfParChunksFuture(const std::vector<T>& vec, Pred p) {
+	const auto chunks = std::thread::hardware_concurrency();
+	const auto chunkLen = vec.size() / chunks;
+
+	std::vector<std::future<std::vector<T>>> tasks(chunks);
+
+	for (size_t i = 0; i < chunks; ++i) {
+		auto startIt = std::next(std::begin(vec), i * chunkLen);
+		auto endIt = std::next(startIt, chunkLen);
+		tasks[i] = std::async(std::launch::async, [=, &p] {
+			std::vector<T> chunkOut;
+			std::copy_if(startIt, endIt, std::back_inserter(chunkOut), p);
+			return chunkOut;
+		});
+	}
+
+	std::vector<T> out;
+
+	for (auto& ft : tasks)
+	{
+		auto part = ft.get();
+		out.insert(out.end(), part.begin(), part.end());
+	}
+
+	// remaining part:
+	if (vec.size() % chunks != 0) {
+		auto startIt = std::next(std::begin(vec), chunks * chunkLen);
+		std::copy_if(startIt, end(vec), std::back_inserter(out), p);
+	}
+
+	return out;
+}
+
+template <typename T, typename Pred>
+auto FilterCopyIfParTransformPush(const std::vector<T>& vec, Pred p) {
+	std::vector<uint32_t> buffer(vec.size());
+	std::vector<uint32_t> idx(vec.size());
+	std::transform(std::execution::par, begin(vec), end(vec), begin(buffer), [&p](const T& elem) {
+		return p(elem) ? 1 : 0;
+		});
+
+	//Print("buffer", buffer);
+	//std::exclusive_scan(std::execution::par, begin(buffer), end(buffer), begin(idx), 0);
+	//Print("idx", idx);
+
+	std::vector<T> out;// (idx.back());
+	//std::vector<size_t> indexes(vec.size());
+	//std::iota(indexes.begin(), indexes.end(), 0);
+
+	for (size_t i = 0; i < vec.size(); ++i)
+	{
+		if (buffer[i])
+		{
+			out.push_back(vec[i]);
+		}
+	}
+
+	//Print("out", out);
 	return out;
 }
 
@@ -142,7 +285,7 @@ auto FilterRangesCopyIfGen(const TCont& vec, Pred p) {
 	return out;
 }
 
-int main() {
+int main(int argc, const char** argv) {
 	const std::vector<std::string> vec{ "Hello", "**txt", "World", "error", "warning", "C++", "****" };
 
 	auto printVec = [](std::string_view intro, const auto& container) {
@@ -213,4 +356,58 @@ int main() {
 		auto filtered = FilterCopyIfGen(vec, [](auto& elem) { return !elem.starts_with('*'); });
 		printVec("FilterCopyIfGen", filtered);
 	}
+
+	// benchmark:
+	std::cout << "\n benchmarks: \n\n";
+
+	const size_t VEC_SIZE = argc > 1 ? atoi(argv[1]) : 62;
+	std::cout << "benchmark vec size: " << VEC_SIZE << '\n';
+
+	std::vector<std::pair<double, double>> testVec(VEC_SIZE);
+	std::ranges::generate(testVec.begin(), testVec.end(), [start = testVec.front()]() mutable {
+		return std::pair { start.first += 1.0, start.second += 0.5 };
+	});
+
+
+	auto test = [](auto& elem) {
+		auto sn = sin(elem.first) * cos(elem.second + 10.0);
+		return sn > 0.0;
+	};
+
+	//auto test = [](int elem) { return elem % 2 == 0; };
+
+	//std::vector<int> testVec(VEC_SIZE);
+	//std::iota(testVec.begin(), testVec.end(), 0);
+
+	RunAndMeasure("FilterCopyIf                ", [&testVec, &test]() {
+		auto filtered = FilterCopyIf(testVec, test);
+		return filtered.size();
+	});
+
+	RunAndMeasure("FilterCopyIfPar             ", [&testVec, &test]() {
+		auto filtered = FilterCopyIfPar(testVec, test);
+		return filtered.size();
+	});
+
+	RunAndMeasure("FilterCopyIfParCompose      ", [&testVec, &test]() {
+		auto filtered = FilterCopyIfParCompose(testVec, test);
+		return filtered.size();
+	});
+
+	RunAndMeasure("FilterCopyIfParComposeSeq   ", [&testVec, &test]() {
+		auto filtered = FilterCopyIfParComposeSeq(testVec, test);
+		return filtered.size();
+		});
+	RunAndMeasure("FilterCopyIfParTransformPush", [&testVec, &test]() {
+		auto filtered = FilterCopyIfParTransformPush(testVec, test);
+		return filtered.size();
+		});
+	RunAndMeasure("FilterCopyIfParChunks       ", [&testVec, &test]() {
+		auto filtered = FilterCopyIfParChunks(testVec, test);
+		return filtered.size();
+		});
+	RunAndMeasure("FilterCopyIfParChunksFuture ", [&testVec, &test]() {
+		auto filtered = FilterCopyIfParChunksFuture(testVec, test);
+		return filtered.size();
+		});
 }
