b16d42815395a5861555f0131fcd7afe2fdb54bb
fenbf <bartlomiej.filipek@gmail.com>
2021-05-20 13:41:45 +0200

    updates

--- filterElements/README.md
+++ filterElements/README.md
@@ -1,3 +1,6 @@
 # filters
 
 code samples for the filters article
+
+* [Implementing Parallel copy_if in C++ - C++ Stories](https://www.cppstories.com/2021/par-copyif/)
+* [12 Different Ways to Filter Containers in Modern C++ - C++ Stories](https://www.cppstories.com/2021/filter-cpp-containers/)
--- filterElements/all_combined/all_combined.cpp
+++ filterElements/all_combined/all_combined.cpp
@@ -1,3 +1,4 @@
+
 #include <algorithm>
 #include <execution>
 #include <functional>
@@ -10,23 +11,52 @@
 #include <string>
 #include <vector>
 #include <type_traits>
+#include <thread>
+#include <atomic>
 
-#include "simpleperf.h"
+#include <chrono>
+
+//#include "simpleperf.h"
+
+#include <vector>
+#include <string>
+
+struct Timings {
+	std::string name;
+	double time{};
+};
+
+std::vector<Timings> g_timings;
+
+template <class T>
+void DoNotOptimizeAway(T&& datum) {
+	datum = datum;
+}
+
+template <typename TFunc> void RunAndMeasure(const char* title, TFunc func)
+{
+	const auto start = std::chrono::steady_clock::now();
+	auto ret = func();
+	const auto end = std::chrono::steady_clock::now();
+	DoNotOptimizeAway(ret);
+
+	const auto t = std::chrono::duration <double, std::milli>(end - start).count();
+	//std::cout << title << ": " << t << " ms, ret: " << ret << '\n';
+
+	g_timings.emplace_back(title, t);
+}
+
 
 // filter - copy only those elements into out that satisfies the predicate
 template <typename TContainer>
 void Print(std::string_view intro, const TContainer& container) {
 	std::cout << intro << ": ";
+
 	for (size_t i = 0; auto && elem : container)
 		std::cout << elem << (++i == container.size() ? "\n" : ", ");
-};
+}
 
-void PrintEx(const std::ranges::range auto& container, auto proj) {
-	for (size_t i = 0; auto && elem : container)
-		std::cout << std::invoke(proj, elem) << (++i == container.size() ? "\n" : ", ");
-};
-
-template <std::floating_point T> 
+template <std::floating_point T>
 T GenRandom(T lower, T upper) {
 	// usage of thread local random engines allows running the generator in concurrent mode
 	thread_local static std::default_random_engine rd;
@@ -39,9 +69,11 @@ T GenRandom(T lower, T upper) {
 template <typename T, typename Pred>
 auto FilterRaw(const std::vector<T>& vec, Pred p) {
 	std::vector<T> out;
+
 	for (auto&& elem : vec)
 		if (p(elem))
 			out.push_back(elem);
+
 	return out;
 }
 
@@ -49,15 +81,18 @@ auto FilterRaw(const std::vector<T>& vec, Pred p) {
 template <typename T, typename Pred>
 auto FilterRawOld(const std::vector<T>& vec, Pred p) {
 	std::vector<T> out;
+
 	for (typename std::vector<T>::const_iterator it = begin(vec); it != end(vec); ++it)
 		if (p(*it))
 			out.push_back(*it);
+
 	return out;
 }
 
 template <typename T, typename Pred>
 auto FilterCopyIf(const std::vector<T>& vec, Pred p) {
 	std::vector<T> out;
+
 	std::copy_if(begin(vec), end(vec), std::back_inserter(out), p);
 	return out;
 }
@@ -68,12 +103,13 @@ struct has_push_back : std::false_type {};
 template <typename T>
 struct has_push_back<T
 	, std::void_t<decltype(std::declval<T>().push_back(std::declval<typename T::value_type>()))>
-	> : std::true_type {};
+> : std::true_type {};
 
 template <typename TCont, typename Pred>
 auto FilterCopyIfGen(const TCont& cont, Pred p) {
 	TCont out;
-	if constexpr(has_push_back<TCont>::value)
+
+	if constexpr (has_push_back<TCont>::value)
 		std::copy_if(begin(cont), end(cont), std::back_inserter(out), p);
 	else
 		std::copy_if(begin(cont), end(cont), std::inserter(out, out.begin()), p);
@@ -84,6 +120,7 @@ auto FilterCopyIfGen(const TCont& cont, Pred p) {
 template <typename T, std::predicate<const T&> Pred>
 auto FilterCopyIfConcepts(const std::vector<T>& vec, Pred p) {
 	std::vector<T> out;
+
 	std::copy_if(begin(vec), end(vec), std::back_inserter(out), p);
 	return out;
 }
@@ -92,6 +129,7 @@ template <typename T, typename Pred>
 auto FilterCopyIfParNaive(const std::vector<T>& vec, Pred p) {
 	std::vector<T> out;
 	std::mutex mut;
+
 	std::for_each(std::execution::par, begin(vec), end(vec),
 		[&out, &mut, p](auto&& elem) {
 			if (p(elem)) {
@@ -107,15 +145,17 @@ template <typename T, typename Pred>
 auto FilterCopyIfParCompose(const std::vector<T>& vec, Pred p) {
 	std::vector<uint8_t> buffer(vec.size());
 	std::vector<uint32_t> idx(vec.size());
+
 	std::transform(std::execution::par, begin(vec), end(vec), begin(buffer), [&p](const T& elem) {
 		return p(elem);
-	});
+		});
 
 #ifdef _DEBUG
 	Print("input\t", buffer);
 	Print("buffer\t", buffer);
 #endif
 	std::exclusive_scan(std::execution::par, begin(buffer), end(buffer), begin(idx), 0);
+
 #ifdef _DEBUG
 	Print("idx\t", idx);
 #endif
@@ -126,10 +166,8 @@ auto FilterCopyIfParCompose(const std::vector<T>& vec, Pred p) {
 
 	std::for_each(std::execution::par, begin(indexes), end(indexes), [&buffer, &vec, &idx, &out](size_t i) {
 		if (buffer[i])
-		{
 			out[idx[i]] = vec[i];
-		}
-	});
+		});
 
 #ifdef _DEBUG
 	Print("out\t", out);
@@ -141,6 +179,7 @@ template <typename T, typename Pred>
 auto FilterCopyIfParComposeSeq(const std::vector<T>& vec, Pred p) {
 	std::vector<uint8_t> buffer(vec.size());
 	std::vector<uint32_t> idx(vec.size());
+
 	std::transform(std::execution::par, begin(vec), end(vec), begin(buffer), [&p](const T& elem) {
 		return p(elem);
 		});
@@ -176,7 +215,7 @@ auto FilterCopyIfParChunks(const std::vector<T>& vec, Pred p) {
 		auto startIt = std::next(std::begin(vec), i * chunkLen);
 		auto endIt = std::next(startIt, chunkLen);
 		std::copy_if(startIt, endIt, std::back_inserter(copiedChunks[i]), p);
-	});
+		});
 
 	std::vector<T> out;
 
@@ -206,7 +245,7 @@ auto FilterCopyIfParChunksFuture(const std::vector<T>& vec, Pred p) {
 			std::vector<T> chunkOut;
 			std::copy_if(startIt, endIt, std::back_inserter(chunkOut), p);
 			return chunkOut;
-		});
+			});
 	}
 
 	std::vector<T> out;
@@ -229,9 +268,10 @@ auto FilterCopyIfParChunksFuture(const std::vector<T>& vec, Pred p) {
 template <typename T, typename Pred>
 auto FilterCopyIfParTransformPush(const std::vector<T>& vec, Pred p) {
 	std::vector<uint32_t> buffer(vec.size());
+
 	std::transform(std::execution::par, begin(vec), end(vec), begin(buffer), [&p](const T& elem) {
 		return p(elem) ? 1 : 0;
-	});
+		});
 
 	std::vector<T> out;
 
@@ -245,6 +285,7 @@ auto FilterCopyIfParTransformPush(const std::vector<T>& vec, Pred p) {
 template <typename T, typename Pred>
 auto FilterRemoveCopyIf(const std::vector<T>& vec, Pred p) {
 	std::vector<T> out;
+
 	std::remove_copy_if(begin(vec), end(vec), std::back_inserter(out), std::not_fn(p));
 	return out;
 }
@@ -252,6 +293,7 @@ auto FilterRemoveCopyIf(const std::vector<T>& vec, Pred p) {
 template <typename T, typename Pred>
 auto FilterRemoveErase(const std::vector<T>& vec, Pred p) {
 	auto out = vec;
+
 	out.erase(std::remove_if(begin(out), end(out), std::not_fn(p)), end(out));
 	return out;
 }
@@ -259,6 +301,7 @@ auto FilterRemoveErase(const std::vector<T>& vec, Pred p) {
 template <typename T, typename Pred>
 auto FilterEraseIf(const std::vector<T>& vec, Pred p) {
 	auto out = vec;
+
 	std::erase_if(out, std::not_fn(p));
 	return out;
 }
@@ -266,6 +309,7 @@ auto FilterEraseIf(const std::vector<T>& vec, Pred p) {
 template <typename TCont, typename Pred>
 auto FilterEraseIfGen(const TCont& cont, Pred p) {
 	auto out = cont;
+
 	std::erase_if(out, std::not_fn(p));
 	return out;
 }
@@ -280,6 +324,7 @@ auto FilterEraseIfGen(const TCont& cont, Pred p) {
 template <typename T, typename Pred>
 auto FilterRangesCopyIf(const std::vector<T>& vec, Pred p) {
 	std::vector<T> out;
+
 	std::ranges::copy_if(vec, std::back_inserter(out), p);
 	return out;
 }
@@ -287,7 +332,59 @@ auto FilterRangesCopyIf(const std::vector<T>& vec, Pred p) {
 template <typename TCont, typename Pred>
 auto FilterRangesCopyIfGen(const TCont& vec, Pred p) {
 	TCont out;
+
 	std::ranges::copy_if(vec, std::back_inserter(out), p);
+	return out;
+}
+
+template<typename T, typename Pred>
+void myProc(const std::vector<T>& vec, std::vector<size_t>& indx, Pred p, size_t thrd, size_t chunk)
+{
+	for (size_t e = thrd; e < vec.size(); e += chunk)
+		if (p(vec[e]))
+			indx.push_back(e);
+}
+
+template<typename T>
+void myMerge(const std::vector<T>& vec, const std::vector<size_t>& indx, std::vector<T>& out, size_t strt)
+{
+	for (size_t i = 0; i < indx.size(); ++i)
+		out[i + strt] = vec[indx[i]];
+}
+
+template<typename T, typename Pred>
+auto MyCopyIf(const std::vector<T>& vec, Pred p)
+{
+	const size_t chunks{ std::thread::hardware_concurrency() };
+	std::vector<std::vector<size_t>> indx(chunks);
+	std::vector<std::thread> threads(chunks);
+
+	for (auto& i : indx)
+		i.reserve(vec.size());
+
+	for (size_t t = 0; t < chunks; ++t)
+		threads[t] = std::thread([&vec, &indx, p, t, chunks]() { myProc(vec, indx[t], p, t, chunks); });
+
+	for (auto& t : threads)
+		t.join();
+
+	size_t res{};
+	std::vector<size_t> start(chunks);
+
+	for (size_t s = 0, strtpos = 0; s < chunks; ++s) {
+		start[s] = strtpos;
+		strtpos += indx[s].size();
+		res += indx[s].size();
+	}
+
+	std::vector<T> out(res);
+
+	for (size_t t = 0; t < chunks; ++t)
+		threads[t] = std::thread([&vec, &indx, &out, &start, t]() {myMerge(vec, indx[t], out, start[t]); });
+
+	for (auto& t : threads)
+		t.join();
+
 	return out;
 }
 
@@ -296,77 +393,89 @@ int main(int argc, const char** argv) {
 
 	auto printVec = [](std::string_view intro, const auto& container) {
 		std::cout << intro << ": ";
+
 		for (size_t i = 0; auto && elem : container)
 			std::cout << elem << (++i == container.size() ? "\n" : ", ");
 	};
 
-	printVec("initial vec:\t", vec);
+	//printVec("initial vec:\t", vec);
+
 	{
 		auto filtered = FilterRaw(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterRaw", filtered);
+		//printVec("FilterRaw", filtered);
 	}
 	{
 		auto filtered = FilterRawOld(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterRawOld", filtered);
+		//printVec("FilterRawOld", filtered);
 	}
 	{
 		auto filtered = FilterCopyIf(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterCopyIf", filtered);
+		//printVec("FilterCopyIf", filtered);
 
 		//filtered = FilterCopyIf(vec, [](auto& elem) { return elem; });
 	}
 	{
 		auto filtered = FilterCopyIfConcepts(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterCopyIfConcepts", filtered);
+		//printVec("FilterCopyIfConcepts", filtered);
 
 		//filtered = FilterCopyIf(vec, [](auto& elem) { return std::pair{ 1, 1 }; });
 	}
 	{
 		auto filtered = FilterCopyIfParNaive(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterCopyIfPar", filtered);
+		//printVec("FilterCopyIfParNaive", filtered);
+	}
+	{
+		auto filtered = FilterCopyIfParCompose(vec, [](auto& elem) { return !elem.starts_with('*'); });
+		//printVec("FilterCopyIfParCompose", filtered);
 	}
 	{
 		auto filtered = FilterRemoveCopyIf(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterRemoveCopyIf", filtered);
+		//printVec("FilterRemoveCopyIf", filtered);
 	}
 	{
 		auto filtered = FilterRemoveErase(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterRemoveErase", filtered);
+		//printVec("FilterRemoveErase", filtered);
 	}
 	{
 		auto filtered = FilterEraseIf(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterEraseIf", filtered);
+		//printVec("FilterEraseIf", filtered);
 	}
 	{
 		auto filtered = FilterRangesCopyIf(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterRangesCopyIf", filtered);
+		//printVec("FilterRangesCopyIf", filtered);
 	}
 	{
 		auto filtered = FilterRangesCopyIfGen(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterRangesCopyIf", filtered);
+		//printVec("FilterRangesCopyIf", filtered);
 	}
 	{
 		std::set<std::string> mySet{ "Hello", "**txt", "World", "error", "warning", "C++", "****" };
-		printVec("initial set", mySet);
+		//printVec("initial set", mySet);
+
 		auto filtered = FilterEraseIfGen(mySet, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterRangesCopyIf", filtered);
+		//printVec("FilterRangesCopyIf", filtered);
 	}
 
 	{
 		std::set<std::string> mySet{ "Hello", "**txt", "World", "error", "warning", "C++", "****" };
 		auto filtered = FilterCopyIfGen(mySet, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterCopyIfGen", filtered);
+		//printVec("FilterCopyIfGen", filtered);
 	}
 
 	{
 		auto filtered = FilterCopyIfGen(vec, [](auto& elem) { return !elem.starts_with('*'); });
-		printVec("FilterCopyIfGen", filtered);
+		//printVec("FilterCopyIfGen", filtered);
+	}
+
+	{
+		auto filtered = MyCopyIf(vec, [](auto& elem) { return !elem.starts_with('*'); });
+		printVec("MyCopyIf", filtered);
 	}
 
 	// benchmark:
-	std::cout << "\n benchmarks: \n\n";
+	//std::cout << "\n benchmarks: \n\n";
 
-	const size_t VEC_SIZE = argc > 1 ? atoi(argv[1]) : 10;
+	const size_t VEC_SIZE = argc > 1 ? atoll(argv[1]) : 10;
 	std::cout << "benchmark vec size: " << VEC_SIZE << '\n';
 
 #ifdef _DEBUG
@@ -378,10 +487,10 @@ int main(int argc, const char** argv) {
 	std::vector<std::pair<double, double>> testVec(VEC_SIZE);
 	std::ranges::generate(testVec.begin(), testVec.end(), []() mutable {
 		return std::pair{ GenRandom(-10.0, 10.0), GenRandom(-10.0, 10.0) };
-	});
+		});
 
 
-	auto test = [](auto& elem) {
+	auto test = [](const auto& elem) {
 		auto sn = sin(elem.first) * cos(elem.second + 10.0);
 		return sn > 0.0;
 	};
@@ -392,43 +501,105 @@ int main(int argc, const char** argv) {
 	RunAndMeasure("transform only seq          ", [&testVec, &buffer, &test]() {
 		std::transform(begin(testVec), end(testVec), begin(buffer), test);
 		return buffer.size();
-	});
+		});
 
 	RunAndMeasure("transform only par          ", [&testVec, &buffer, &test]() {
 		std::transform(std::execution::par, begin(testVec), end(testVec), begin(buffer), test);
 		return buffer.size();
-	});
+		});
 
 	RunAndMeasure("FilterCopyIf                ", [&testVec, &test]() {
 		auto filtered = FilterCopyIf(testVec, test);
 		return filtered.size();
-	});
+		});
 
 	RunAndMeasure("FilterCopyIfParNaive        ", [&testVec, &test]() {
 		auto filtered = FilterCopyIfParNaive(testVec, test);
 		return filtered.size();
-	});
+		});
 
 	RunAndMeasure("FilterCopyIfParCompose      ", [&testVec, &test]() {
 		auto filtered = FilterCopyIfParCompose(testVec, test);
 		return filtered.size();
-	});
+		});
 
 	RunAndMeasure("FilterCopyIfParComposeSeq   ", [&testVec, &test]() {
 		auto filtered = FilterCopyIfParComposeSeq(testVec, test);
 		return filtered.size();
 		});
+
 	RunAndMeasure("FilterCopyIfParTransformPush", [&testVec, &test]() {
 		auto filtered = FilterCopyIfParTransformPush(testVec, test);
 		return filtered.size();
 		});
+
 	RunAndMeasure("FilterCopyIfParChunks       ", [&testVec, &test]() {
 		auto filtered = FilterCopyIfParChunks(testVec, test);
 		return filtered.size();
 		});
+
 	RunAndMeasure("FilterCopyIfParChunksFuture ", [&testVec, &test]() {
 		auto filtered = FilterCopyIfParChunksFuture(testVec, test);
 		return filtered.size();
 		});
-	
+
+	RunAndMeasure("FilterRaw                   ", [&testVec, &test]() {
+		auto filtered = FilterRaw(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterRawOld                ", [&testVec, &test]() {
+		auto filtered = FilterRawOld(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterCopyIfConcepts        ", [&testVec, &test]() {
+		auto filtered = FilterCopyIfConcepts(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterRemoveCopyIf          ", [&testVec, &test]() {
+		auto filtered = FilterRemoveCopyIf(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterRemoveErase           ", [&testVec, &test]() {
+		auto filtered = FilterRemoveErase(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterEraseIf               ", [&testVec, &test]() {
+		auto filtered = FilterEraseIf(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterRangesCopyif          ", [&testVec, &test]() {
+		auto filtered = FilterRangesCopyIf(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterRangesCopyIfGen       ", [&testVec, &test]() {
+		auto filtered = FilterRangesCopyIfGen(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterEraseIfGen            ", [&testVec, &test]() {
+		auto filtered = FilterEraseIfGen(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("FilterCopyIfGen             ", [&testVec, &test]() {
+		auto filtered = FilterCopyIfGen(testVec, test);
+		return filtered.size();
+		});
+
+	RunAndMeasure("MyCopyIf                     ", [&testVec, &test]() {
+		auto filtered = MyCopyIf(testVec, test);
+		return filtered.size();
+		});
+
+	std::sort(g_timings.begin(), g_timings.end(), [](const auto& i1, const auto& i2) {return i1.time < i2.time; });
+
+	for (const auto& t : g_timings)
+		std::cout << t.name << ' ' << t.time << '\n';
 }
